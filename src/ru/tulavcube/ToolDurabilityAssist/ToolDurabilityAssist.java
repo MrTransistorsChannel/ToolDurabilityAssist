1a535c61f76db7ab8df3952b7f9362c436aa6336
MrTransistor <mrtransistor0108@gmail.com>
2021-10-01 10:55:48 +0300

    Added warning/critical thresholds Added config version check

--- src/ru/tulavcube/ToolDurabilityAssist/ToolDurabilityAssist.java
+++ src/ru/tulavcube/ToolDurabilityAssist/ToolDurabilityAssist.java
@@ -1,5 +1,8 @@
 package ru.tulavcube.ToolDurabilityAssist;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,15 +12,20 @@ import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.tulavcube.ToolDurabilityAssist.commands.tooldurabilityassist;
 
+import java.io.File;
+
 public class ToolDurabilityAssist extends JavaPlugin implements Listener {
 
-    private int _percent;
+    private int _percentWarning;
+    private int _damageCritical;
+    private int configVersion = 2;
 
     @Override
     public void onEnable() {
         super.onEnable();
         getLogger().info("ToolDurabilityAssist plugin started");
-        saveDefaultConfig();
+
+        checkConfigFile();
         updateSettings();
 
         getCommand("tooldurabilityassist").setExecutor(new tooldurabilityassist(this));
@@ -37,7 +45,21 @@ public class ToolDurabilityAssist extends JavaPlugin implements Listener {
         Player player = e.getPlayer();
         ItemStack damagedItem = e.getItem();
 
-        if (checkDurability(damagedItem, _percent)) return;
+        if (e.getDamage() == 0) return;
+        if (!checkDurabilityAbs(damagedItem, _damageCritical)) {
+            String customName = damagedItem.getItemMeta().getDisplayName();
+            String itemName;
+            if (customName.isBlank()) {
+                itemName = damagedItem.getType().toString().replace('_', ' ').toLowerCase();
+                itemName = itemName.substring(0, 1).toUpperCase() + itemName.substring(1);
+            } else itemName = customName;
+
+            player.sendTitle(" ", itemName + " durability is low", 10, 60, 10);
+            player.playSound(player.getLocation(), "block.chest.open", 1, 2F);
+
+            return;
+        }
+        if (checkDurabilityPercent(damagedItem, _percentWarning)) return;
 
         String customName = damagedItem.getItemMeta().getDisplayName();
         String itemName;
@@ -46,20 +68,42 @@ public class ToolDurabilityAssist extends JavaPlugin implements Listener {
             itemName = itemName.substring(0, 1).toUpperCase() + itemName.substring(1);
         } else itemName = customName;
 
-        player.sendTitle(" ", itemName + " durability is low", 10, 60, 10);
-        player.playSound(player.getLocation(), "block.chest.open", 1, 2F);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.
+                fromLegacyText(itemName + " durability is low"));
+    }
 
+    private void checkConfigFile(){
+        FileConfiguration config = getConfig();
+        if(config == null) {
+            saveDefaultConfig();
+            return;
+        }
+        if(configVersion == config.getInt("config-version")){
+            File configFile = new File(getDataFolder(), "config.yml");
+            configFile.delete();
+            saveDefaultConfig();
+        }
     }
 
     public void updateSettings() {
-        _percent = getConfig().getInt("percentage");
+        _percentWarning = getConfig().getInt("warning");
+        _damageCritical = getConfig().getInt("critical");
     }
 
-    private boolean checkDurability(ItemStack item, float percentDamage) {
+    private boolean checkDurabilityPercent(ItemStack item, float percentLeft) {
         if (item != null && item.getItemMeta() instanceof Damageable) {
             int maxDurability = item.getType().getMaxDurability();
             int damage = ((Damageable) item.getItemMeta()).getDamage();
-            return !((float) (maxDurability - damage) / maxDurability * 100 < percentDamage);
+            return !((float) (maxDurability - damage) / maxDurability * 100 < percentLeft);
+        }
+        return true;
+    }
+
+    private boolean checkDurabilityAbs(ItemStack item, int durabilityLeft) {
+        if (item != null && item.getItemMeta() instanceof Damageable) {
+            int maxDurability = item.getType().getMaxDurability();
+            int damage = ((Damageable) item.getItemMeta()).getDamage();
+            return !(maxDurability - damage < durabilityLeft);
         }
         return true;
     }
--- src/ru/tulavcube/ToolDurabilityAssist/commands/tooldurabilityassist.java
+++ src/ru/tulavcube/ToolDurabilityAssist/commands/tooldurabilityassist.java
@@ -22,20 +22,34 @@ public class tooldurabilityassist implements TabExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player && !sender.isOp()) {
             sender.sendMessage(ChatColor.RED + "You need to be an operator to run this command");
-        } else {
-            if (args.length == 3 && args[0].equalsIgnoreCase("percentage") && args[1].equalsIgnoreCase("set")) {
-                _plg.getConfig().set("percentage", Integer.parseInt(args[2]));
-                _plg.saveConfig();
-                _plg.updateSettings();
-                sender.sendMessage(ChatColor.DARK_GREEN + "Saved!");
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("percentage") && args[1].equalsIgnoreCase("get")) {
-                sender.sendMessage(ChatColor.DARK_GREEN + "Current durability threshold is set to "
-                        + _plg.getConfig().getInt("percentage") + "%");
-            } else {
-                return false;
+        } else if (args.length == 3) {
+            if (args[1].equalsIgnoreCase("set")) {
+                if (args[0].equalsIgnoreCase("warningPercentage")) {
+                    _plg.getConfig().set("warning", Integer.parseInt(args[2]));
+                    _plg.saveConfig();
+                    _plg.updateSettings();
+                    sender.sendMessage(ChatColor.DARK_GREEN + "Saved!");
+                    return true;
+                } else if (args[0].equalsIgnoreCase("criticalLevel")) {
+                    _plg.getConfig().set("critical", Integer.parseInt(args[2]));
+                    _plg.saveConfig();
+                    _plg.updateSettings();
+                    sender.sendMessage(ChatColor.DARK_GREEN + "Saved!");
+                    return true;
+                } else return false;
             }
+        } else if (args.length == 2) {
+            if (args[1].equalsIgnoreCase("get")) {
+                if (args[0].equalsIgnoreCase("warningPercentage")) {
+                    sender.sendMessage(ChatColor.DARK_GREEN + "Warning is set to " + _plg.getConfig().getInt("warning") + "%");
+                    return true;
+                } else if (args[0].equalsIgnoreCase("criticalLevel")) {
+                    sender.sendMessage(ChatColor.DARK_GREEN + "Critical level is set to " + _plg.getConfig().getInt("critical"));
+                    return true;
+                } else return false;
+            } else return false;
         }
-        return true;
+        return false;
     }
 
     @Override
@@ -46,7 +60,8 @@ public class tooldurabilityassist implements TabExecutor {
             ArrayList<String> arr = new ArrayList<>();
 
             if (args.length == 1) {
-                arr.add("percentage");
+                arr.add("warningPercentage");
+                arr.add("criticalLevel");
             } else if (args.length == 2) {
                 arr.add("get");
                 arr.add("set");
